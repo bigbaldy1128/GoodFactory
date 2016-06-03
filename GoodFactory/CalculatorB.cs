024b81a3821e90057b306740c37e6b295020b768
wjz <wjz@qhct.com>
2016-06-03 10:55:59 +0800

    Init

---
+++ GoodFactory/CalculatorB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodFactory
+{
+    public class CalculatorB : ICalculator
+    {
+        public int Calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GoodFactory/TestFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodFactory
+{
+    public class TestFactory : ITestFactory
+    {
+        Dictionary<CalculatorEnum, Type> dic;
+
+        public TestFactory(Dictionary<string,string> calculatorDic)
+        {
+            dic = ConvertStringsDictToDictOfTypes(calculatorDic);
+            CheckIfAllValuesFromDictImplementsProperInterface();
+        }
+
+        public ICalculator GetCalculator(CalculatorEnum calculatorEnum)
+        {
+            Type calculator;
+
+            if (!dic.TryGetValue(calculatorEnum, out calculator))
+            {
+                throw new NotImplementedException("There is no implementation of IAccountDiscountCalculatorFactory interface for given Account Status");
+            }
+
+            return (ICalculator)Activator.CreateInstance(calculator);
+        }
+
+        private void CheckIfAllValuesFromDictImplementsProperInterface()
+        {
+            foreach (var item in dic)
+            {
+                if (!typeof(ICalculator).IsAssignableFrom(item.Value))
+                {
+                    throw new ArgumentException("The type: " + item.Value.FullName + " does not implement IAccountDiscountCalculatorFactory interface!");
+                }
+            }
+        }
+
+        private Dictionary<CalculatorEnum, Type> ConvertStringsDictToDictOfTypes(Dictionary<string, string> calculatorDic)
+        {
+            return calculatorDic.ToDictionary(
+                                    t => (CalculatorEnum)Enum.Parse(typeof(CalculatorEnum), t.Key, true),
+                                    t => Type.GetType(t.Value));
+        }
+    }
+}
